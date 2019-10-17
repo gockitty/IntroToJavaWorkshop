f16427c174b93a6a981f82a5f910a4625636227b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-16 18:28:57 -0700

    day 5

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -1,5 +1,5 @@
 package section3;
-
+int
 import javax.swing.JOptionPane;
 
 import com.sun.speech.freetts.Voice;
@@ -9,15 +9,44 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("spell madlibs");
 		// 2. Catch the user's answer in a String
-
+String answer = JOptionPane.showInputDialog("spelling");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if (answer.equalsIgnoreCase("madlibs")) {
+	speak("correct");
+	
+}
 		// 4. Otherwise say "wrong"
-
+else {
+	
+	speak("wrong");
+}
 		// 5. repeat the process for other words
 		
+speak("spell chemicals");
+
+String answer2 = JOptionPane.showInputDialog("spelling");
+
+if (answer2.equalsIgnoreCase("chemicals")) {
+	speak("correct");
+}
+else {
+	
+	speak ("wrong");
+}
+speak ("spell Rrobot");
+
+String answer3 = JOptionPane.showInputDialog("spelling");
+
+if (answer3.equalsIgnoreCase("Robot")) {
+speak ("correct");	
+}
+else {
+speak ("wrong");
+}
+
+}
 	}
 
 	static void speak(String words) {
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,19 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+String cow =JOptionPane.showInputDialog( "Do you know how to write code?" );
 		// 2. If they say "yes", tell them they will rule the world.
-
+if (cow.equalsIgnoreCase("yes")) {
+	
+	JOptionPane.showMessageDialog(null,"YOU CAN RULE THE WORLD");
+	
+}
 		// 3. Otherwise, wish them good luck washing dishes.
+else {
+	
+	JOptionPane.showMessageDialog(null,"good luck washing dishes");
+	
+}
 
 	}
 }
-
